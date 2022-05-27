3cde86505f8410aecc7880f707715c6e8d3dd8bc
TomooAkamatsu <t.akmt@ymail.ne.jp>
2022-05-27 20:54:56 +0900

    initial commit

---
+++ src/main/java/com/example/sma/SmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sma/SmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
