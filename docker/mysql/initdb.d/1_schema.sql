6c71acdfe767a0b6679153167f825d51a8f318c1
TomooAkamatsu <t.akmt@ymail.ne.jp>
2022-06-02 20:06:34 +0900

    環境変数の設定、DB名変更処理

---
+++ docker/mysql/initdb.d/1_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name varchar(20),
+    last_name varchar(20),
+    PRIMARY KEY (id)
+);
---
+++ docker/mysql/initdb.d/2_initdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(first_name, last_name) VALUES('Ichiro', 'Suzuki');
+INSERT INTO users(first_name, last_name) VALUES('Taro', 'Tanaka');
