7d090a979d5b6bea4add867ff2cd7b8729a2bc7b
TomooAkamatsu <t.akmt@ymail.ne.jp>
2022-06-19 13:33:20 +0900

    Mapperクラスのテストを一部修正

--- src/test/java/com/example/sma/infrastructure/employee/EmployeeRepositoryTest.java
+++ src/test/java/com/example/sma/infrastructure/employee/EmployeeRepositoryTest.java
@@ -76,13 +76,14 @@ class EmployeeRepositoryTest {
 
     @Test
     public void 従業員idに紐づく1件の更新ができること() {
-        employeeRepository.updateEmployee("age", "60", 1);
+        employeeRepository.updateEmployee("last_name", "赤松", 1);
 
         List<Employee> actualEmployeeList = employeeRepository.findAllEmployee();
 
-        assertThat(actualEmployeeList).contains(
-                new Employee(1, "岸田", "文雄", "Kishida", "Fumio", "1957-07-29", 60, "男", "090-1111-1111", "kishida@hoge.com", "2020-01-01", null, new WorkingForm(1, "正社員"))
-        );
+        assertThat(actualEmployeeList)
+                .contains(
+                        new Employee(1, "赤松", "文雄", "Kishida", "Fumio", "1957-07-29", 64, "男", "090-1111-1111", "kishida@hoge.com", "2020-01-01", null, new WorkingForm(1, "正社員"))
+                );
     }
 
     //   このケースはフォームクラスを介さないのでここから例外を投げて処理するかフロントで制御したい
@@ -98,7 +99,17 @@ class EmployeeRepositoryTest {
         employeeRepository.deleteEmployee(1);
         List<Employee> actualEmployeeList = employeeRepository.findAllEmployee();
 
-        assertThat(actualEmployeeList).hasSize(4);
+        assertThat(actualEmployeeList)
+                .hasSize(4)
+                .contains(
+                        new Employee(2, "菅", "義偉", "Suga", "Yoshihide", "1948-12-06", 73, "男", "090-2222-2222", "suga@hoge.com", "2020-02-01", null, new WorkingForm(2, "正社員(時短)")),
+                        new Employee(3, "安倍", "晋三", "Abe", "Shinzo", "1954-09-21", 67, "男", "090-3333-3333", "abe@hoge.com", "2020-03-01", null, new WorkingForm(1, "正社員")),
+                        new Employee(4, "野田", "佳彦", "Noda", "Yoshihiko", "1957-05-20", 65, "男", "090-4444-4444", "noda@hoge.com", "2020-04-01", null, new WorkingForm(1, "正社員")),
+                        new Employee(5, "菅", "直人", "Kan", "Naoto", "1946-10-10", 75, "男", "090-5555-5555", "kan@hoge.com", "2020-05-01", null, new WorkingForm(3, "パート"))
+                )
+                .doesNotContain(
+                        new Employee(1, "岸田", "文雄", "Kishida", "Fumio", "1957-07-29", 64, "男", "090-1111-1111", "kishida@hoge.com", "2020-01-01", null, new WorkingForm(1, "正社員"))
+                );
     }
 
     @Test
